6e72b63c48f47bc3c2dad41b8b8c464a056670a2
Martin Rebane <mrtnrbn@users.noreply.github.com>
2016-10-12 15:25:32 +0300

    copysin failid

---
+++ login.php
@@ -0,0 +1,171 @@
+
+
+<?php 
+	// login j2tab eposti meelde, v2ljad kohustuslikud
+	//signup 4 v2lja(oma idee)-esimene kodutoo lisa 2 v2lja(nt kaal ja pikkus)
+	//andmete salvestamine ja n2itamine oma idee j2rgi
+	require("/home/martreba/config.php");
+	require("functions.php");
+	
+	//var_dump($_GET);
+	
+	//echo "<br>";
+	
+	//var_dump($_POST);
+	
+	//MUUTUJAD
+	$signupEmailError = "*";
+	$signupEmail = "";
+	
+	//kas keegi vajutas nuppu ja see on olemas
+	
+	if (isset ($_POST["signupEmail"])) {
+		
+		//on olemas
+		// kas epost on tühi
+		if (empty ($_POST["signupEmail"])) {
+			
+			// on tühi
+			$signupEmailError = "* Väli on kohustuslik!";
+			
+		} else {
+			// email on olemas ja õige
+			$signupEmail = $_POST["signupEmail"];
+			
+		}
+		
+	} 
+	
+	$signupPasswordError = "*";
+	
+	if (isset ($_POST["signupPassword"])) {
+		
+		if (empty ($_POST["signupPassword"])) {
+			
+			$signupPasswordError = "* Väli on kohustuslik!";
+			
+		} else {
+			
+			// parool ei olnud tühi
+			
+			if ( strlen($_POST["signupPassword"]) < 8 ) {
+				
+				$signupPasswordError = "* Parool peab olema vähemalt 8 tähemärkki pikk!";
+				
+			}
+			
+		}
+		
+	}
+	
+	//vaikimisi väärtus
+	$gender = "";
+	
+	if (isset ($_POST["gender"])) {
+		if (empty ($_POST["gender"])) {
+			$genderError = "* Väli on kohustuslik!";
+		} else {
+			$gender = $_POST["gender"];
+		}
+		
+	} 
+	
+	
+	
+	
+	if ( $signupEmailError == "*" AND
+		 $signupPasswordError == "*" &&
+		 isset($_POST["signupEmail"]) && 
+		 isset($_POST["signupPassword"]) 
+	  ) {
+		
+		//vigu ei olnud, kõik on olemas	
+		echo "Salvestan...<br>";
+		echo "email ".$signupEmail."<br>";
+		echo "parool ".$_POST["signupPassword"]."<br>";
+		
+		$password = hash("sha512", $_POST["signupPassword"]);
+		
+		echo $password."<br>";
+		
+		signup($signupEmail, $password);
+		
+		
+	}
+	
+	// kas kasutaja tahab sisse logida
+	if ( isset($_POST["loginEmail"])&&
+		 isset($_POST["loginPassword"])&&
+		 !empty($_POST["loginEmail"])&&
+		 !empty($_POST["loginPassword"])
+		)	{
+		$notice=login($_POST["loginEmail"], $_POST["loginPassword"]);
+		}
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sisselogimise leht</title>
+	</head>
+	<body>
+
+		<h1>Logi sisse</h1>
+		
+		<form method="POST" >
+			
+			<label>E-post</label><br>
+			<input name="loginEmail" type="email">
+			
+			<br><br>
+
+			<input name="loginPassword" placeholder="Parool" type="password">
+			
+			<br><br>
+			
+			<input type="submit" value="Logi sisse">
+		
+		</form>
+		
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST" >
+			
+			<label>E-post</label><br>
+			<input name="signupEmail" type="email" value="<?=$signupEmail;?>"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+
+			<input name="signupPassword" placeholder="Parool" type="password"> <?php echo $signupPasswordError; ?>
+			
+			<br><br>
+					
+			<?php if ($gender == "female") { ?>
+				<input type="radio" name="gender" value="female" checked> female<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="female" > female<br>
+			<?php } ?>
+			
+			<?php if ($gender == "male") { ?>
+				<input type="radio" name="gender" value="male" checked> male<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="male" > male<br>
+			<?php } ?>
+			
+			
+			<?php if ($gender == "other") { ?>
+				<input type="radio" name="gender" value="other" checked> other<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="other" > other<br>
+			<?php } ?>
+			
+			<input type="submit" value="Loo kasutaja">
+		
+		</form>
+		<form action="age">
+			  <input type="checkbox" name="age" value="age"> Olen vahemalt 18 aastat vana.<br>
+			  <input type="submit" value="Submit">
+		</form>
+
+	</body>
+</html>
